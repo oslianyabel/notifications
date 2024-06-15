48657c97aa362c836fcaa508e9c71d5cfbe9f283
oslianyabel <oslianyabel@gmail.com>
2024-06-14 22:11:32 -0400

    environ fixed

--- notifications/settings.py
+++ notifications/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,5 +113,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TWILIO_ACCOUNT_SID = os.environ('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.environ('TWILIO_AUTH_TOKEN')
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
